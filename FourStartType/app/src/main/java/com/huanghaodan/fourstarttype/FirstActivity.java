b16fd43f29906f200b4959364ca6fd9484df3cde
黄浩丹 <1310189872@qq.com>
2019-10-04 10:16:14 +0800

    第一种启动模式：standard

---
+++ FourStartType/app/src/main/java/com/huanghaodan/fourstarttype/FirstActivity.java
@@ -0,0 +1,26 @@
+package com.huanghaodan.fourstarttype;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+public class FirstActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Log.d("FirstActivity",this.toString());
+        setContentView(R.layout.first_layout);
+        Button button1=(Button)findViewById(R.id.button_1);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(FirstActivity.this, FirstActivity.class);
+                startActivity(intent);
+            }
+        });
+    }}
